15686c003b874cccf90c54d36ffac609cdb59604
Derek Schultz <dschultz@brinkster.com>
2012-12-26 14:43:45 -0700

    Move repo code to package_repo.rb. Default recipe is now just an include and package install

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,21 +15,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-case node["platform_family"]
-when "rhel", "centos"
-	include_recipe "yum"
-	yum_key "RPM-GPG-KEY-percona" do
-		url "http://www.percona.com/downloads/RPM-GPG-KEY-percona"
-		action :add
-	end
-
-	yum_repository "percona" do
-		name "CentOS-Percona"
-		url "http://repo.percona.com/centos/#{node["platform_version"].split('.')[0]/os/#{node["kernel"]["machine"]}/"
-		key "RPM-GPG-KEY-percona"
-		action :add
-	end
-end
+include_recipe "percona-toolkit::package_repo"
 
 package "percona-toolkit"
