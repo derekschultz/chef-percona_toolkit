9159886fa184f7a1f216947ba836653a4b6d0b7b
Derek Schultz <dschultz@brinkster.com>
2012-12-26 16:01:51 -0700

    Fixed annoying whitespace in name field. Corrected supported/tested platforms.

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name						 "percona-toolkit"
+name		 "percona-toolkit"
 maintainer       "Derek Schultz"
 maintainer_email "dschultz@brinkster.com"
 license          "Apache 2.0"
-description      "Installs/Configures percona-toolkit"
+description      "Installs Percona apt/yum repository and percona-toolkit package."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-%w{redhat centos ubuntu}.each do |os|
+%w{ centos debian redhat ubuntu }.each do |os|
 	supports os
 end
 
@@ -14,4 +14,4 @@ depends "apt"
 depends "yum"
 
 recipe "percona-toolkit",	"Installs the Percona Toolkit software."
-recipe "percona-toolkit::package_repo",	"Sets up the package repo and installs dependent packages."
+recipe "percona-toolkit::package_repo",	"Sets up the package yum/apt repository."
